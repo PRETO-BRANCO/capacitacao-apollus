99323517c264197d98c1b180a94e2931df933fff
Gustavo Preto Benvenutti <gustavo.benvenutti@apollusehs.com.br>
2021-07-14 10:19:06 -0300

    Enviando projeto spring

---
+++ apollus-study/src/test/java/com/study/apollus/ApollusStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.apollus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApollusStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
